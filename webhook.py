b6b46ad1f9e2631e36b7f447545c7f64e1f5da56
= <=>
2025-06-11 17:17:42 +0530

    added ai

--- webhook.py
+++ webhook.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, request
 import logging
+from google import genai
 
 app = Flask(__name__)
 
@@ -22,6 +23,21 @@ def webhook():
         data = request.get_json()
         logging.basicConfig(level=logging.INFO)
         logging.info("Received: %s", data)
+        client = genai.Client(api_key="AIzaSyDD6et8LEH_gD-R64zt9X7Wnk_UCCVOXBw")
+        answerFromAi = client.models.generate_content(model="gemini-2.0-flash",contents=data["entry"][0]["changes"][0]["value"]["messages"][0]["text"]["body"],)
+        url = "https://graph.facebook.com/v23.0/700474829809739/messages"
+        headers = {
+            "Authorization": "Bearer EAA6iFxZC6E44BO4IJaYRkA6mGpZBOIUSzA9FYoZB3C0ZBMN6mlrZCr7hIU9CKadsoJ4AJ8nuWN2hqHWYv100LLp5hPP1VCVFN7hVCqf1AsxrXQrjSCw4f8PAdEkNjeN6ZACFGFoxV47dBEfAToO2KZCHT2VVqaIdlCpDOWSthd0zZBWpyKakVcy9JhfR6DhExMiGYXqHfIeSYzlIZCIvK9AvllA3bFlRNtJrbHksZD",
+            "Content-Type": "application/json"
+        }
+        dataHaveToSend = {
+            "messaging_product": "whatsapp",
+            "to": data["entry"][0]["changes"][0]["value"]["messages"][0]["from"],  # Receiver
+            "type": "text",
+            "text": {"body": answerFromAi}
+        }
+        r = requests.post(url, headers=headers, json=dataHaveToSend)
+        logging.info("Received: %s", str(r.status_code)+str( r.text))
         return "OK", 200
 
 if __name__ == '__main__':
